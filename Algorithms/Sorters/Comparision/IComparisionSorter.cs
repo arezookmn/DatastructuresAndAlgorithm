fe2db72d6809671d3049058f3737cf2b0ed1f8c5
arezookmn <arezookmn1@gmail.com>
2024-10-02 18:52:36 +0330

    Create insertion sort class and interface for all comparison sort algorithms

---
+++ Algorithms/Sorters/Comparision/IComparisionSorter.cs
@@ -0,0 +1,6 @@
+﻿namespace Algorithms.Sorters.Comparision;
+public interface IComparisionSorter<T>
+{
+    void Sort(T[] array, IComparer<T> comparer);
+}
+
---
+++ Algorithms/Sorters/Comparision/InsertionSort.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms.Sorters.Comparision;
+public class InsertionSort<T> : IComparisionSorter<T>
+{
+    public void Sort(T[] array, IComparer<T> comparer)
+    {
+        int n = array.Length;
+        for (int i = 1; i < n; i++)
+        {
+            T key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && comparer.Compare(key, array[j]) < 0)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+    }
+}
